d25a9f81df27cc7650df36a61ff0ae3d0f2eb455
uowllnh <uowllnh@gmail.com>
2026-02-12 23:42:52 +0900

    chore: 세부사항 수정

--- yupddeok-web/src/mocks/handlers.ts
+++ yupddeok-web/src/mocks/handlers.ts
@@ -1,3 +1,4 @@
+import { count } from "console";
 import { http, HttpResponse, delay } from "msw";
 
 const menus = [
@@ -339,6 +340,80 @@ const menus = [
 ] as const;
 
 
+const stores = [
+
+  {
+    id: "store-001",
+    name: "강남역점",
+    distance: "310m",
+    open: true
+  },
+
+  {
+    id: "store-002",
+    name: "언주역점",
+    distance: "1.4km",
+    open: true
+  },
+
+  {
+    id: "store-003",
+    name: "양재점",
+    distance: "1.4km",
+    open: true
+  },
+{
+    id: "store-004",
+    name: "역삼직영점",
+    distance: "1.4km",
+    open: false
+  },
+
+  {
+    id: "store-005",
+    name: "교대점",
+    distance: "1.5km",
+    open: true
+  },
+
+  {
+    id: "store-006",
+    name: "반포점",
+    distance: "1.6km",
+    open: true
+  },
+
+  {
+    id: "store-007",
+    name: "대치점",
+    distance: "2km",
+    open: true
+  },
+
+  {
+    id: "store-008",
+    name: "신사역점",
+    distance: "2.2km",
+    open: false
+  },
+
+  {
+    id: "store-009",
+    name: "신반포점",
+    distance: "2.5km",
+    open: false
+  },
+
+  {
+    id: "store-010",
+    name: "강남구청역점",
+    distance: "2.5km",
+    open: true
+  },
+
+] as const;
+
+
 const SPICE_OPTIONS = {
   A: {
   required: true,
@@ -428,8 +503,6 @@ export const handlers = [
     const id = params.id as string;
 
     const menu = menus.find((m) => m.id === id);
-    console.log("MSW HIT id:", id);
-    console.log("menus:", menus.map(m => m.id));
     
     // 없으면 404
     if (!menu) {
@@ -438,6 +511,14 @@ export const handlers = [
         { status: 404 }
       );
     }
+  
+    http.get("/api/stores/:id", ({ params }) => {
+  const { id } = params;
+  const store = stores.find((m) => m.id === id);
+
+  return HttpResponse.json(store);
+});
+
 
     const spiceType = CATEGORY_TO_SPICE_TYPE[menu.category] ; // 기본값
     const spice = SPICE_OPTIONS[spiceType];
@@ -451,10 +532,7 @@ export const handlers = [
     ...TOPPING_OPTIONS[t],
   }));
     
-
-
-
-    return HttpResponse.json({...menu,spice,toppingChoices,});
+    return HttpResponse.json({...menu,spice,toppingChoices});
   }),
 
   
